8809d166f159a3f5316aad32b042721c5731e069
Kyrylo Prylipko <kyrylo.pr@oxoix.com>
2026-07-18 19:29:42 +0200

    mouse, idt, interrupts

--- boot/boot.asm
+++ boot/boot.asm
@@ -22,7 +22,7 @@ section .code
 
     mov bx, 0x1000
     mov ah, 0x02
-    mov al, 21
+    mov al, 25
     mov ch, 0x00
     mov dh, 0x00
     mov cl, 0x02
--- boot/main.c
+++ boot/main.c
@@ -6,11 +6,19 @@ int start()
    char str1[] = "Welcome to GraphOS!\n\nText rendered by custom library";
    char *p = str1;
 
+   base = (unsigned int)&isr1;
+   base12 = (unsigned int)&isr12;
+
+   InitialiseMouse();
+   InitialiseIDT();
 
    while(1)
    {
         ClearScreen(181.0f / 255.0f * 16.0f, 232.0f / 255.0f * 32.0f, 255.0f / 255.0f * 16.0f);
         DrawString(getArialCharacter, font_arial_width, font_arial_height, p, 100, 100, 0, 0, 0);
+
+        DrawRect(x, y, 10, 10, 0, 0, 0);
+
         Flush();
    }
 }
