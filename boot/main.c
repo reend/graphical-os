5432b4bf18e7ce2fcd33911dc34bcd509045efc4
Kyrylo Prylipko <kyrylo.pr@oxoix.com>
2026-07-18 20:44:28 +0200

    buffer fix

--- boot/main.c
+++ boot/main.c
@@ -10,6 +10,10 @@ int start()
    char str1[] = "Welcome to Graph Operation System!";
    char *p = str1;
 
+   char characterBuffer[1000] = "\0";
+   char* characterBufferPointer = characterBuffer;
+   int characterBufferLength = 0;
+
    base = (unsigned int)&isr1;
    base12 = (unsigned int)&isr12;
 
@@ -18,8 +22,24 @@ int start()
 
    while(1)
    {
+        char character = ProcessScancode(Scancode);
+
+        if (backspace_pressed == TRUE) {
+            characterBuffer[characterBufferLength - 1] = '\0';
+            characterBufferLength--;
+            backspace_pressed = FALSE;
+            Scancode = -1;
+        } 
+        else if (character != '\0')
+        {
+         characterBuffer[characterBufferLength++] = character;
+         characterBuffer[characterBufferLength] = '\0';
+         Scancode = -1;
+        }
+
         ClearScreen(181.0f / 255.0f * 16.0f, 232.0f / 255.0f * 32.0f, 255.0f / 255.0f * 16.0f);
-        DrawString(getArialCharacter, font_arial_width, font_arial_height, p, 100, 100, 0, 0, 0);
+
+        DrawString(getArialCharacter, font_arial_width, font_arial_height, characterBufferPointer, 100, 100, 0, 0, 0);
 
         // DrawRect(x, y, 10, 10, 0, 0, 0);
         DrawMouse(x, y, 16, 100.0 / 255.0 * 32, 100.0 / 255.0 * 16);
